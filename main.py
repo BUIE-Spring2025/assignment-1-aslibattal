23e8d39609e834c060983b067d8db9ee8804cd56
aslibattal <asli.battal@std.bogazici.edu.tr>
2025-03-09 13:57:54 +0300

    I added a for loop into my function, updated pytest, and tried to run the tests

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def int_to_roman(num):
     :return: A string representing the Roman numeral of the integer.
     """
     dict_roman = { 1: "I", 4:"IV", 5 :"V", 9:"IX", 10: "X", 40:"XL", 50:"L", 90:"XC", 100:"C", 400:"CD", 500:"D", 900: "CM",1000:"M"}
+    roman= ""
+    for number, symbol in dict_roman.items():
+        while num >= number:
+            roman += symbol
+            num -= number
+    return roman
