b1eb2c1195e4eab80f9b5fb7a05d731f298d5063
aslibattal <asli.battal@std.bogazici.edu.tr>
2025-03-10 13:07:08 +0300

    My tests are not working but my results come out true.

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
-    dict_roman = { 1: "I", 4:"IV", 5 :"V", 9:"IX", 10: "X", 40:"XL", 50:"L", 90:"XC", 100:"C", 400:"CD", 500:"D", 900: "CM",1000:"M"}
+    dict_roman = { 1000:"M", 900:"CM", 500: "D", 400: "CD", 100: "C", 90:"XC",50:"L", 40:"XL",10:"X", 9:"IX", 5:"V",4:"IV", 1: "I"}
     roman= ""
     for number, symbol in dict_roman.items():
         while num >= number:
             roman += symbol
             num -= number
     return roman
+print(int_to_roman(3))
+print(int_to_roman(58))
+print(int_to_roman(400))
+print(int_to_roman(1994))
+print(int_to_roman(3999))
